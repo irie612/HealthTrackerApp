07b85c8115b4eeedd86d4c97fb48cad3eec9f9dc
Elijah Rose <elijahr0001@gmail.com>
2021-03-16 23:34:05 +0000

    Add login field labels, font and field error effect

--- src/sample/controllers/LoginController.java
+++ src/sample/controllers/LoginController.java
@@ -3,6 +3,8 @@ package sample.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.paint.Color;
 
 
 public class LoginController {
@@ -16,12 +18,19 @@ public class LoginController {
     private void loginBtnOnClick() {
         String username = usernameField.getText();
         String password = passwordField.getText();
+
+        //check if username field is empty
         if (username.equals("")) {
-
-        } else if (password.equals("")) {
-
+            usernameField.setEffect(new DropShadow(5, Color.RED));
         } else {
+            usernameField.setEffect(null);
+        }
 
+        //check if password field is empty
+        if (password.equals("")) {
+            passwordField.setEffect(new DropShadow(5, Color.RED));
+        } else {
+            passwordField.setEffect(null);
         }
         System.out.println(username + ", " + password);
     }
