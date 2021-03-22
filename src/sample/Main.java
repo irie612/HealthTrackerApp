fb8121cbef2465a31fabfd4e07abec6a61e9c5fb
irie612 <57913132+irie612@users.noreply.github.com>
2021-03-22 15:58:48 +0000

    added functionality of buttons in mealtrackercontroller

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,16 +8,18 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    Scene mealScene, exerciseScene, dashboardScene, loginUser, registerScene, userScene;
+    public static Stage window;
+
+    public static Scene mealScene, exerciseScene, dashboardScene, loginUser, registerScene, accountScene, groupScene;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        window = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("resources/views/MealTracker.fxml"));
         Scene mealScene = new Scene(root);
         primaryStage.setScene(mealScene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
--- src/sample/controllers/MealTrackerController.java
+++ src/sample/controllers/MealTrackerController.java
@@ -7,8 +7,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextArea;
+import sample.Main;
 
 public class MealTrackerController implements Initializable {
 ObservableList list= FXCollections.observableArrayList();
@@ -23,7 +24,15 @@ ObservableList list= FXCollections.observableArrayList();
     void initialize() {
     }
 
-    @FXML private ChoiceBox mealType;
+    @FXML private ChoiceBox<String> mealType;
+
+    @FXML private Button switchHome;
+
+    @FXML private Button switchExercise;
+
+    @FXML private Button switchGroup;
+
+    @FXML private Button switchAccount;
 
     //@FXML private TextArea myMeal;
 
@@ -31,6 +40,12 @@ ObservableList list= FXCollections.observableArrayList();
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // Choicebox meal type
         mealType.getItems().addAll("Breakfast", "Lunch", "Dinner", "Snack", "Drink");
+
+        switchHome.setOnAction(event -> Main.window.setScene(Main.dashboardScene));
+        switchExercise.setOnAction(event -> Main.window.setScene(Main.exerciseScene));
+        switchGroup.setOnAction(event -> Main.window.setScene(Main.groupScene));
+        switchAccount.setOnAction(event -> Main.window.setScene(Main.accountScene));
+
 
         // my meal
         //myMeal.setText("MY MEAL : " );
