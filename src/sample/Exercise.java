6bbfe3fabb76309ff8c8af15a6f2118ef987dc50
irie612 <57913132+irie612@users.noreply.github.com>
2021-04-26 11:07:00 +0100

    added Exercise.java
    Signed-off-by: irie612 <57913132+irie612@users.noreply.github.com>

---
+++ src/sample/Exercise.java
@@ -0,0 +1,38 @@
+package sample;
+
+public class Exercise {
+    private float distance, duration, caloriesBurned;
+    private int steps;
+
+    public float getDistance() {
+        return distance;
+    }
+
+    public void setDistance(float distance) {
+        this.distance = distance;
+    }
+
+    public float getDuration() {
+        return duration;
+    }
+
+    public void setDuration(float duration) {
+        this.duration = duration;
+    }
+
+    public float getCaloriesBurned() {
+        return caloriesBurned;
+    }
+
+    public void setCaloriesBurned(float caloriesBurned) {
+        this.caloriesBurned = caloriesBurned;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
+
+    public void setSteps(int steps) {
+        this.steps = steps;
+    }
+}
