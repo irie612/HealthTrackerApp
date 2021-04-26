ebcb4e8f1b70e19b8527edb2d9f9a6c534cf3ddb
irie612 <57913132+irie612@users.noreply.github.com>
2021-04-26 12:22:32 +0100

    Exercise.java class variables finished. Enum incomplete
    Signed-off-by: irie612 <57913132+irie612@users.noreply.github.com>

--- src/sample/Exercise.java
+++ src/sample/Exercise.java
@@ -1,31 +1,48 @@
 package sample;
 
-public class Exercise {
-    private float distance, duration, caloriesBurned;
-    private int steps;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
-    public float getDistance() {
+public class Exercise {
+    private double distance, caloriesBurned, duration;
+    private int steps;
+    private LocalDate date;
+    private LocalTime time;
+    private exerciseType type;
+
+    public enum exerciseType {}
+
+    public Exercise(double distance, double caloriesBurned, double duration, int steps, exerciseType type) {
+        this.distance = distance;
+        this.caloriesBurned = caloriesBurned;
+        this.duration = duration;
+        this.steps = steps;
+        this.date = LocalDate.now();
+        this.time = LocalTime.now();
+    }
+
+    public double getDistance() {
         return distance;
     }
 
-    public void setDistance(float distance) {
+    public void setDistance(double distance) {
         this.distance = distance;
     }
 
-    public float getDuration() {
-        return duration;
-    }
-
-    public void setDuration(float duration) {
-        this.duration = duration;
-    }
-
-    public float getCaloriesBurned() {
+    public double getCaloriesBurned() {
         return caloriesBurned;
     }
 
-    public void setCaloriesBurned(float caloriesBurned) {
+    public void setCaloriesBurned(double caloriesBurned) {
         this.caloriesBurned = caloriesBurned;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
     }
 
     public int getSteps() {
@@ -34,5 +51,42 @@ public class Exercise {
 
     public void setSteps(int steps) {
         this.steps = steps;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(int day, int month, int year) {
+        this.date = LocalDate.of(year, month, day);
+    }
+
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(int hour, int minute) throws IllegalArgumentException {
+        if ((hour < 24 && minute < 60) && (hour >= 0 && minute >= 0))
+            this.time = LocalTime.of(hour, minute);
+        else
+            throw new IllegalArgumentException("Argument out of range");
+    }
+
+    public exerciseType getType() {
+        return type;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Exercise{" +
+                "type=" + type +
+                ", distance=" + distance +
+                ", caloriesBurned=" + caloriesBurned +
+                ", duration=" + duration +
+                ", steps=" + steps +
+                ", date=" + date +
+                ", time=" + time +
+                '}';
     }
 }
