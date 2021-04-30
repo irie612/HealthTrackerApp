314d1c0addaf665bc9ef254f36a995949e107a23
Elijah Rose <elijahr0001@gmail.com>
2021-04-30 14:45:09 +0100

    add Trie and TrieNode data structure

---
+++ src/sample/utilities/Trie.java
@@ -0,0 +1,285 @@
+package sample.utilities;
+
+import java.util.*;
+
+public class Trie implements Collection<String>, Iterable<String> {
+
+    private TrieNode root;
+    private int size;
+
+
+    public Trie() {
+        root = new TrieNode(' ', false, true);
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        if (o.getClass() == String.class) {
+
+            String key = (String) o;
+            key = key.toLowerCase();
+
+            TrieNode temp = root;
+
+            for (char c : key.toCharArray()) {
+                TrieNode next = temp.getOffspring(c);
+
+                if (next == null)
+                    return false;
+                temp = next;
+            }
+            return true;
+        } else
+            return false;
+    }
+
+
+    @Override
+    public Iterator<String> iterator() {
+        return new TrieIterator(this);
+    }
+
+    @Override
+    public Object[] toArray() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean add(String k) {
+        TrieNode temp = root;
+
+        k = k.toLowerCase();
+
+        for (char c : k.toCharArray()) {
+
+            TrieNode next = temp.getOffspring(c);
+
+            if (next == null) {
+                next = new TrieNode(c);
+                temp.setOffspring(next);
+
+            }
+
+            temp = next;
+        }
+
+        if (!temp.isKey()) {
+            temp.setKey(true);
+            size++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends String> c) {
+
+        for (String s : c) {
+            boolean added = add(s);
+            if (!added) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    static class TrieIterator implements Iterator<String> {
+
+        private Trie t;
+        private int c;
+        private TrieNode currentNode;
+        private Stack<TrieNode> s = new Stack<>();
+        private HashSet<TrieNode> seen = new HashSet<>();
+
+        public TrieIterator(Trie t) {
+            this.t = t;
+            this.c = 0;
+            s.push(t.root);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return c < t.size();
+        }
+
+
+        @Override
+        public String next() {
+            StringBuilder sb = new StringBuilder();
+            String key = "";
+
+            if (c < t.size()) {
+
+                while (!s.isEmpty()) {
+                    currentNode = s.peek();
+
+
+                    if (currentNode.isKey() && !seen.contains(currentNode)) { // if node is a key
+
+                        //retrieve key
+                        for (TrieNode n : s) {
+                            if (!n.isRoot()) {  //ignore root
+                                char v = n.getValue();
+//                                System.out.println(n.getValue());
+                                sb.append(v);
+                            }
+                        }
+
+                        key = sb.toString();
+                        c++; //key found
+                    }
+
+                    seen.add(currentNode);
+
+                    //find a child node which has not been seen yet
+                    Iterator<TrieNode> it = currentNode.getAllOffspring().iterator();
+                    TrieNode next = null;
+                    boolean found = false;
+                    while (!found && it.hasNext()) {
+                        next = it.next();
+                        if (!seen.contains(next))
+                            found = true;
+                    }
+
+                    if (!found)
+                        s.pop();  //no offspring found, go back to parent node
+                    else
+                        s.push(next); //add next offspring
+
+                    if (!key.equals("")) {
+                        return key;  //if word is found the  return word
+                    }
+                }
+
+            }
+            throw new NoSuchElementException();
+        }
+    }
+
+    public TrieNode getNodeOfPrefix(String prefix) {
+        TrieNode temp = root;
+
+        for (char c : prefix.toCharArray()) {
+
+
+            if (temp.getOffspring(c) != null) {
+
+                temp = temp.getOffspring(c);
+            } else {
+                throw new NoSuchElementException();
+            }
+        }
+        return temp;
+    }
+
+    public ArrayList<String> getKeysFromPrefix(String prefix) {
+        ArrayList<String> keys = new ArrayList<>();
+
+
+        TrieNode temp = getNodeOfPrefix(prefix);
+
+        Stack<TrieNode> s = new Stack<>();
+        HashSet<TrieNode> seen = new HashSet<>();
+
+        StringBuilder sb = new StringBuilder();
+
+        s.push(temp);
+
+        String key = "";
+
+        while (!s.empty()) {
+
+            temp = s.peek();
+
+            if (temp.isKey() && !seen.contains(temp)) { // if node is a key
+
+                boolean first = true;
+                //retrieve key
+                for (TrieNode n : s) {
+
+                    if (!first) {
+                        if (!n.isRoot()) {  //ignore root
+                            char v = n.getValue();
+                            sb.append(v);
+                        }
+                    } else {
+                        first = false;
+                    }
+                }
+
+                key = sb.toString();
+            }
+
+            seen.add(temp);
+
+            //find a child node which has not been seen yet
+            Iterator<TrieNode> it = temp.getAllOffspring().iterator();
+            TrieNode next = null;
+            boolean found = false;
+
+            while (!found && it.hasNext()) {
+                next = it.next();
+                if (!seen.contains(next))
+                    found = true;
+            }
+
+            if (!found)
+                s.pop();  //no offspring found, go back to parent node
+            else
+                s.push(next); //add next offspring
+
+            if (!key.equals("")) {
+                keys.add(prefix + key);
+                key = "";
+                sb.delete(0, sb.length());
+            }
+
+        }
+        return keys;
+    }
+
+}
+
---
+++ src/sample/utilities/TrieNode.java
@@ -0,0 +1,106 @@
+package sample.utilities;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class TrieNode implements Comparable<TrieNode> {
+
+    private char value;
+    private boolean isKey;
+    private boolean isRoot;
+
+    private HashMap<Character, TrieNode> offspring;
+
+    public TrieNode(char value, boolean isKey) {
+        this.value = value;
+        this.isRoot = false;
+        this.isKey = isKey;
+        offspring = new HashMap<>();
+    }
+
+    public TrieNode(char value, boolean isKey, boolean isRoot) {
+        this.value = value;
+        this.isKey = isKey;
+        this.isRoot = isRoot;
+        offspring = new HashMap<>();
+    }
+
+    public TrieNode(char value) {
+        this.value = value;
+        this.isKey = false;
+        this.isRoot = false;
+        offspring = new HashMap<>();
+    }
+
+    public int offspringSize() {
+        return offspring.size();
+    }
+
+    public ArrayList<TrieNode> getAllOffspring() {
+        return new ArrayList<>(offspring.values());
+    }
+
+    public TrieNode getOffspring(char x) {
+
+        TrieNode node = null;
+
+        if (offspring.containsKey(x)) {
+            node = offspring.get(x);
+        }
+
+        return node;
+    }
+
+    public boolean setOffspring(TrieNode newNode) {
+        boolean added = false;
+
+        if (getOffspring(newNode.value) == null) {
+            offspring.put(newNode.value, newNode);
+            added = true;
+        }
+        return added;
+    }
+
+    public void setKey(boolean key) {
+        isKey = key;
+    }
+
+    public boolean isKey() {
+        return isKey;
+    }
+
+    public char getValue() {
+        return this.value;
+    }
+
+    public void setValue(char value) {
+        this.value = value;
+    }
+
+    public boolean isRoot() {
+        return isRoot;
+    }
+
+    @Override
+    public int compareTo(TrieNode o) {
+        return Character.compare(this.getValue(), o.getValue());
+    }
+
+    public static void main(String[] args) {
+        TrieNode root = new TrieNode(' ', false, true);
+        TrieNode t1 = new TrieNode('H');
+        TrieNode t2 = new TrieNode('i', true);
+        TrieNode t3 = new TrieNode('i', true);
+        t3.setKey(true);
+
+        t2.setOffspring(t3);
+        t1.setOffspring(t2);
+        root.setOffspring(t1);
+
+        boolean isSame = t3.equals(t2);
+        System.out.println(isSame);
+
+
+    }
+}
