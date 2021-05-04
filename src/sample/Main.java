9cd94b56f0b7363c6633b2541dc515dc1fa43a8b
joshua-yeung <joshua.yeung78@gmail.com>
2021-05-04 11:10:23 +0100

    Database

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("resources/views/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("resources/views/UserGroupView.fxml"));
+        primaryStage.setTitle("Health Tracker");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
---
+++ src/sample/UserDatabase.java
@@ -0,0 +1,52 @@
+package sample;
+
+import java.io.*;
+
+public class UserDatabase extends Database2<Users> {
+
+    public UserDatabase(String url) throws IOException{
+
+        super(url);
+    }
+
+    public UserDatabase(String url, String delimiter) throws IOException{
+
+        super(url, delimiter);
+    }
+
+    @Override
+    public void insert(Users users) throws IOException {
+
+        fw = new BufferedWriter(new FileWriter(url));
+        data.add(users);
+
+        String row = users.getUsername() + delimiter + users.getEmail() + delimiter + users.getWeight() + delimiter +
+                    users.getHeight();
+        fw.write(row);
+        fw.newLine();
+        fw.close();
+    }
+
+    @Override
+    public void loadElements() throws IOException {
+
+        fileReader = new BufferedReader(new FileReader(url));
+        String line;
+
+        while((line = fileReader.readLine()) != null){
+
+            String[] tokens = line.split(delimiter);
+            String username = tokens[0].trim();
+            String email = tokens[1].trim();
+            String w = tokens[2].trim();
+            String h = tokens[3].trim();
+
+            double weight = Double.parseDouble(w);
+            double height = Double.parseDouble(h);
+
+            data.add(new Users(username, email, height, weight));
+        }
+
+        fileReader.close();
+    }
+}
