1a8ac1d86f097f3da6bd491340e9825d122f6724
joshua-yeung <joshua.yeung78@gmail.com>
2021-05-07 13:08:30 +0100

    member class and database

---
+++ src/sample/UserGroupMember.java
@@ -0,0 +1,26 @@
+package sample;
+
+public class UserGroupMember {
+
+    private String groupName;
+    private String memberUsername;
+    private int memberScore;
+
+    public UserGroupMember(String groupName, String memberUsername, int memberScore) {
+        this.groupName = groupName;
+        this.memberUsername = memberUsername;
+        this.memberScore = memberScore;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public String getMemberUsername() {
+        return memberUsername;
+    }
+
+    public int getMemberScore() {
+        return memberScore;
+    }
+}
